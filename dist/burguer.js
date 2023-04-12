0d21c07214ec2bf36604903fde96a27c4ea679e1
Sofia <sofiagarciafranco20\@gmail.com>
2023-04-11 22:11:56 -0500

    .

--- dist/burguer.js
+++ dist/burguer.js
@@ -1,5 +1,12 @@
 let burguer=document.getElementById("burguer");
 let mobile=document.querySelectorAll(".mobile")[0];
+let menuUlLi=document.querySelectorAll(".menuUlLi");
 burguer.addEventListener('click', ()=> {
     mobile.classList.toggle("showMobile");
+});
+
+menuUlLi.forEach((elemento)=>{
+elemento.addEventListener('click', ()=>{
+    mobile.classList.toggle("showMobile");
+})
 })
